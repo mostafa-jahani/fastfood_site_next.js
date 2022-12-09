d03265b5a43060ab91de5ae5d16be0cacfb228b7
Mostafa Jahani <mm.mostafa.mm@gmail.com>
2022-12-10 02:16:05 +0330

    start project and set configs

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '@/styles/globals.css'
+import {useEffect} from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect( () => {
+    import ('bootstrap/dist/js/bootstrap.bundle');
+  }, [])
+
   return <Component {...pageProps} />
 }
 
