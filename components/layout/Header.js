2a5e4fd9a3eda11689e4a6429a342b80cd36af9e
Mostafa Jahani <mm.mostafa.mm@gmail.com>
2022-12-12 19:15:28 +0330

    create about page and contact page

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 
 const Header = () => {
+    const router = useRouter();
 
     return (
-        <div>
+        <div className={router.pathname === '/' ? '' : 'sub_page'}>
             <div className="hero_area">
                 <div className="bg-box">
                     <Image src={'/images/hero-bg.jpg'} fill alt="hero-image" />
@@ -12,11 +16,9 @@ const Header = () => {
                 <header className="header_section">
                     <div className="container">
                         <nav className="navbar navbar-expand-lg custom_nav-container">
-                            <a className="navbar-brand" href="index.html">
-                                <span>
-                                    فست فود
-                                </span>
-                            </a>
+                            <Link href="/" className="navbar-brand">
+                                    <span>فست فود</span>
+                            </Link>
 
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -26,17 +28,17 @@ const Header = () => {
 
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                 <ul className="navbar-nav mx-auto">
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="index.html">صفحه اصلی</a>
+                                    <li className={router.pathname === '/' ? 'nav-item active' : 'nav-item'}>
+                                        <Link href="/" className="nav-link">صفحه اصلی</Link>
                                     </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="menu.html">منو</a>
+                                    <li className={router.pathname === '/menu' ? 'nav-item active' : 'nav-item'}>
+                                        <Link href="/menu" className="nav-link">منو</Link>
                                     </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="about.html">درباره ما</a>
+                                    <li className={router.pathname === '/about' ? 'nav-item active' : 'nav-item'}>
+                                        <Link href="/about" className="nav-link">درباره ما</Link>
                                     </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="contact.html">تماس باما</a>
+                                    <li className={router.pathname === '/contact' ? 'nav-item active' : 'nav-item'}>
+                                        <Link href="/contact" className="nav-link">تماس با ما</Link>
                                     </li>
                                 </ul>
                                 <div className="user_option">
@@ -55,101 +57,103 @@ const Header = () => {
                     </div>
                 </header>
 
-                <section className="slider_section">
-                    <div id="customCarousel1" className="carousel slide" data-bs-ride="carousel">
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-7 col-lg-6">
-                                            <div className="detail-box">
-                                                <h2 className="mb-3 fw-bold">
-                                                    لورم ایپسوم متن ساختگی
-                                                </h2>
-                                                <p>
-                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                                    از
-                                                    طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                                    سطرآنچنان که
-                                                    لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                                    بهبود
-                                                    ابزارهای کاربردی می باشد.
-                                                </p>
-                                                <div className="btn-box">
-                                                    <a href="" className="btn1">
-                                                        سفارش
-                                                    </a>
+                {router.pathname === '/' &&
+                    (<section className="slider_section">
+                        <div id="customCarousel1" className="carousel slide" data-bs-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-md-7 col-lg-6">
+                                                <div className="detail-box">
+                                                    <h2 className="mb-3 fw-bold">
+                                                        لورم ایپسوم متن ساختگی
+                                                    </h2>
+                                                    <p>
+                                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+                                                        از
+                                                        طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                                                        سطرآنچنان که
+                                                        لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
+                                                        بهبود
+                                                        ابزارهای کاربردی می باشد.
+                                                    </p>
+                                                    <div className="btn-box">
+                                                        <a href="" className="btn1">
+                                                            سفارش
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-md-7 col-lg-6">
+                                                <div className="detail-box">
+                                                    <h2 className="mb-3 fw-bold">
+                                                        لورم ایپسوم متن ساختگی
+                                                    </h2>
+                                                    <p>
+                                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+                                                        از
+                                                        طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                                                        سطرآنچنان که
+                                                        لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
+                                                        بهبود
+                                                        ابزارهای کاربردی می باشد.
+                                                    </p>
+                                                    <div className="btn-box">
+                                                        <a href="" className="btn1">
+                                                            سفارش
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-md-7 col-lg-6">
+                                                <div className="detail-box">
+                                                    <h2 className="mb-3 fw-bold">
+                                                        لورم ایپسوم متن ساختگی
+                                                    </h2>
+                                                    <p>
+                                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+                                                        از
+                                                        طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                                                        سطرآنچنان که
+                                                        لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
+                                                        بهبود
+                                                        ابزارهای کاربردی می باشد.
+                                                    </p>
+                                                    <div className="btn-box">
+                                                        <a href="" className="btn1">
+                                                            سفارش
+                                                        </a>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="carousel-item">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-7 col-lg-6">
-                                            <div className="detail-box">
-                                                <h2 className="mb-3 fw-bold">
-                                                    لورم ایپسوم متن ساختگی
-                                                </h2>
-                                                <p>
-                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                                    از
-                                                    طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                                    سطرآنچنان که
-                                                    لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                                    بهبود
-                                                    ابزارهای کاربردی می باشد.
-                                                </p>
-                                                <div className="btn-box">
-                                                    <a href="" className="btn1">
-                                                        سفارش
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="carousel-item">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-7 col-lg-6">
-                                            <div className="detail-box">
-                                                <h2 className="mb-3 fw-bold">
-                                                    لورم ایپسوم متن ساختگی
-                                                </h2>
-                                                <p>
-                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                                    از
-                                                    طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                                    سطرآنچنان که
-                                                    لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                                    بهبود
-                                                    ابزارهای کاربردی می باشد.
-                                                </p>
-                                                <div className="btn-box">
-                                                    <a href="" className="btn1">
-                                                        سفارش
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div className="container">
+                                <ol className="carousel-indicators">
+                                    <li data-bs-target="#customCarousel1" data-bs-slide-to="0" className="active"></li>
+                                    <li data-bs-target="#customCarousel1" data-bs-slide-to="1"></li>
+                                    <li data-bs-target="#customCarousel1" data-bs-slide-to="2"></li>
+                                </ol>
                             </div>
                         </div>
-                        <div className="container">
-                            <ol className="carousel-indicators">
-                                <li data-bs-target="#customCarousel1" data-bs-slide-to="0" className="active"></li>
-                                <li data-bs-target="#customCarousel1" data-bs-slide-to="1"></li>
-                                <li data-bs-target="#customCarousel1" data-bs-slide-to="2"></li>
-                            </ol>
-                        </div>
-                    </div>
 
-                </section>
+                    </section>)
+                }
 
             </div>
         </div>
---
+++ pages/contact.js
@@ -0,0 +1,32 @@
+import dynamic from "next/dynamic";
+import ContactForm from "@/components/homePage/contact/ContactForm";
+const Map = dynamic(() => import('@/components/homePage/contact/Map'), { ssr: false })
+
+
+const ContactPage = () => {
+    return (
+        <section className="book_section layout_padding">
+            <div className="container">
+                <div className="heading_container">
+                    <h2>
+                        تماس با ما
+                    </h2>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="form_container">
+                            <ContactForm />
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="map_container ">
+                            <Map />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ContactPage;
